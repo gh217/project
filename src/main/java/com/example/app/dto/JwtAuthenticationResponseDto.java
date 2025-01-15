6e9d2be3e55e660bb6661081c2bcbdbfbac104bd
mohamed.ghanem <mohamed.ghanem@diracsystems.com>
2025-01-15 16:29:38 +0200

    done

---
+++ src/main/java/com/example/app/dto/JwtAuthenticationResponseDto.java
@@ -0,0 +1,13 @@
+package com.example.app.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class JwtAuthenticationResponseDto {
+
+    private String token;
+    private String refreshToken;
+
+}
---
+++ src/main/java/com/example/app/dto/RefreshTokenRequestDto.java
@@ -0,0 +1,10 @@
+package com.example.app.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class RefreshTokenRequestDto {
+    private String token;
+}
